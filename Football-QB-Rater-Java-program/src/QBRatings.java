c98acfe13af9b1e0f803b96bbf921a9abc4a122c
Dara <Dara@128.4.109.145>
2017-02-19 20:40:31 -0500

    QB Football Rater

--- Football-QB-Rater-Java-program/src/QBRatings.java
+++ Football-QB-Rater-Java-program/src/QBRatings.java
@@ -48,19 +48,51 @@ public class QBRatings {
 			ATT = input.nextDouble();
 			System.out.print("\n");
 		}
-	}
-
-	public double QBRating(int ATT, int COMP, int YDS, int TD, int INTCP) {
-
-		if (ATT == 0)
-			return 0;
-		{
-			double a = ((double) COMP / ATT - 0.3) * 5.0;
-			double b = ((double) YDS / ATT - 3.0) * 0.25;
-			double c = ((double) TD / ATT) * 20.0;
-			double d = 2.375 - ((double) INTCP / ATT * 25.0);
-			double passerRating = ((a + b + c + d) / 6.0) * 100.0;
-			return (passerRating < 0) ? 0.0 : passerRating;
+		
+		if (ATT == 0){
+			System.out.print(0);
 		}
+			double a = ((double) COMP / ATT - 0.3) * 5.0;
+			if (a>2.375){
+				a = 2.375;
+			}
+			if (a<0){
+				a = 0;
+			}
+			double b = ((double) YDS / ATT - 3.0) * 0.25;
+			if (b>2.375){
+				b = 2.375;
+			}
+			if (b<0){
+				b = 0;
+			}
+			double c = ((double) TD / ATT) * 20.0;
+			if (c>2.375){
+				c = 2.375;
+			}
+			if (c<0){
+				c = 0;
+			}
+			double d = 2.375 - ((double) INTCP / ATT * 25.0);
+			if (d>2.375){
+				d = 2.375;
+			}
+			if (d<0){
+				d = 0;
+			}
+			double passerRating = ((a + b + c + d) / 6.0) * 100.0;
+			if (passerRating < 0){
+				System.out.print(0);
+			}
+			if (0 > passerRating){
+				System.out.print(0);
+			}
+			if (158.3 > passerRating){
+				System.out.printf("%3.1f",passerRating);
+			}
+			if (158.3 <= passerRating){
+				System.out.print(158.3);
+			}
 	}
 }
+
